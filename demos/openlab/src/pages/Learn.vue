0af99ee0f6790f36dc3db1081dd5ff7025cc64bf
haiyang <hhy5277@126.com>
2018-09-20 16:37:43 +0800

    Create Learn.vue

---
+++ demos/openlab/src/pages/Learn.vue
@@ -0,0 +1,421 @@
+<template>
+  <div class="page">
+    <!--banner开始-->
+    <banner :banner-msg="bannerMsg"></banner>
+    <!--banner结束-->
+
+    <!--learn搜索-->
+    <div class="learn-search content">
+      <el-row :gutter="0">
+        <el-col :span="8">
+          <div class="grid-content">
+            <div class="cell font-small" @click="sortFormName(5,'default')">
+              <span :class="{'ask':sort.isA == 5}">默认排序</span>
+            </div>
+            <div class="cell font-small">
+              <span :class="{'ask':sort.isA == 1||sort.isA == 2}" @click="sortFormName('','visitors')">按访问量排序</span>
+              <span class="caret-wrapper">
+                <i class="sort-caret ascending" :class="{'active':sort.isA == 1}" @click="sortMake(1,'asc','visitors')"></i>
+                <i class="sort-caret descending" :class="{'active':sort.isA == 2}" @click="sortMake(2,'desc','visitors')"></i>
+              </span>
+            </div>
+            <div class="cell font-small">
+              <span :class="{'ask':sort.isA == 3||sort.isA == 4}" @click="sortFormName('','create_time')">按上线时间排序</span>
+              <span class="caret-wrapper">
+                <i class="sort-caret ascending" :class="{'active':sort.isA == 3}" @click="sortMake(3,'asc','create_time')"></i>
+                <i class="sort-caret descending" :class="{'active':sort.isA == 4}" @click="sortMake(4,'desc','create_time')"></i>
+              </span>
+            </div>
+          </div>
+        </el-col>
+        <el-col style="width: 420px;margin-left: 380px">
+          <div class="grid-content">
+            <el-select :style="selectBg"
+                       v-model="sort.productId"
+                       placeholder="请选择产品"
+                       size="small"
+                       @change="productChage"
+                       style="width: 140px;">
+              <el-option value="" label="全部">全部</el-option>
+              <el-option
+                v-for="item in options"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id">
+              </el-option>
+            </el-select>
+            <el-input :style="inputBg" placeholder="请输入关键字" size="small" v-model="sort.keyWords"></el-input>
+            <el-button :style="searchBg" type="primary" class="search-button" @click="search">搜索</el-button>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <!--learn搜索end-->
+
+    <!--learn内容-->
+    <div class="learn-source content">
+        <div class="case-item" v-for="(item,index) in courses" :key="item.courseId" :class="{'case-item-border':index<courses.length-1}">
+          <el-row :gutter="0">
+            <el-col :span="13">
+              <div class="grid-content">
+                <img :src="item.courseLogo" style="float:left;"/>
+              </div>
+            </el-col>
+            <el-col :span="11">
+              <div class="grid-content font-color">
+                <div class="font-big" style="margin-bottom: 30px;">{{item.courseTitle}}</div>
+                <div class="font-normal"><span class="course-content">访问人数：</span>{{item.visitors}}</div>
+                <div class="font-normal"><span class="course-content">上线时间：</span>{{item.createTime&&item.createTime.substring(0,10)}}</div>
+                <div class="font-normal"><span class="course-content">课程类型：</span>{{item.productName}}</div>
+                <div class="font-normal course-desc">
+                  <span class="course-content">课程介绍：</span>{{item.courseDesc}}
+                </div>
+                <router-link :to="{path:'/learnDetail/'+item.courseId}"><el-button :style="experienceBg">开始学习</el-button></router-link>
+              </div>
+            </el-col>
+          </el-row>
+      </div>
+    </div>
+    <!--learn内容end-->
+  </div>
+</template>
+
+<script type="text/javascript">
+  import Banner from '../components/Banner'
+  export default {
+    name:'Learn',
+    data() {
+      return {
+        bannerMsg:{
+          bgBtn:{
+            // backgroundImage:'url(' + require('../../static/images/study/banner.png') + ')',
+            backgroundImage:'url(' + this.GLOBAL.BASE_URL + 'course/banner' +')',
+            backgroundPosition:'center',
+            height: '300px'
+          },
+          title:"为开发者打造专业的在线学习平台",
+          desc:"提供中兴通讯多产品能力开发培训资源，您可以随时随地学习中兴产品集成开发",
+          file:"",
+          textAlign:{
+            'text-align':"left"
+          },
+          hWidth:{}
+        },
+        sort:{
+          isA:5,//排序
+          keyWords:"",//关键字
+          productId:"",//产品类型
+        },
+        params:{
+          name:"",
+          productId:"",
+          sort:""
+        },
+        options: [],
+        courses:[],
+        selectBg:{backgroundImage:'url(' + require('../../static/images/api/200.png') + ')'},
+        inputBg:{backgroundImage:'url(' + require('../../static/images/api/140.png') + ')'},
+        experienceBg:{backgroundImage:'url(' + require('../../static/images/api/206.png') + ')'},
+        searchBg:{backgroundImage:'url(' + require('../../static/images/api/60.png') + ')'}
+      }
+    },
+    components:{
+      Banner
+    },
+    mounted:function() { //钩子函数
+      this.getCourseList();//获取所有课程信息
+      this.getProductList();
+      window.scrollTo(0,0);//初始化页面在最顶部
+    },
+    methods:{
+      getCourseList:function () {
+        let _this = this;
+        this.$http.get('course/list', {
+        }).then(function (response) {
+          if(0 == response.data.returnCode && response.data.data) {
+            if(0 == response.data.returnCode && response.data.data) {
+              _this.courses = response.data.data?response.data.data.map((item) => {
+                return {
+                  courseId: item.courseId,
+                  courseTitle:item.courseTitle,
+                  visitors:item.visitors,
+                  createTime:item.createTime,
+                  courseType:item.courseType,
+                  productName:item.productName,
+                  courseDesc:item.courseDesc,
+                  courseLogo:_this.GLOBAL.BASE_URL+"course/logo/"+ item.courseId
+                }
+              }):[];
+            }else{
+              _this.$message('获取课程信息失败！');
+            }
+          }else{
+            _this.$message('获取课程信息失败！');
+          }
+        }).catch(function (error) {});
+      },
+      getProductList:function () {
+        let _this = this;
+        this.$http.get('product/listbytype?category=-1', {
+        }).then(function (response) {
+          console.log(response)
+            if(0 == response.data.returnCode && response.data.data) {
+              _this.options = response.data.data?response.data.data.map((item) => {
+                return {
+                  id: item.productId,
+                  name: item.productName
+                }
+              }):[];
+            }else{
+              _this.$message('获取产品列表失败！');
+            }
+        });
+      },
+      sortMake:function (num,type,sort) {
+        let _this = this;
+        _this.sort.keyWords = "";
+        _this.sort.productId = "";
+        _this.sort.isA = num ;
+        _this.params={
+          name:"",
+          productId:"",
+          sort:sort+" "+type
+        }
+        _this.ajaxCourseList(_this.params);
+      },
+      productChage(){
+        let _this = this;
+        _this.sort.keyWords = "";
+        _this.sort.isA = 5;
+        _this.params={
+          name:"",
+          productId:_this.sort.productId,
+          sort:""
+        }
+        _this.ajaxCourseList(_this.params);
+      },
+      sortFormName(num,sort){
+        let _this = this;
+        _this.sort.keyWords = "";
+        _this.sort.productId = "";
+        _this.params={
+          name:"",
+          productId:"",
+          sort:""
+        }
+        var type = "";
+        if(sort == "default"){
+          _this.params.sort = "";
+          _this.sort.isA = 5;
+        }else if(sort == "visitors"){
+          if(_this.sort.isA == 1) {
+            _this.sort.isA = 2;
+            type = "desc";
+          }else{
+            _this.sort.isA = 1;
+            type = "asc";
+          }
+          _this.params.sort = sort+" "+type;
+        }else{
+          if(_this.sort.isA == 3) {
+            type = "desc";
+            _this.sort.isA = 4;
+          }else{
+            type = "asc";
+            _this.sort.isA = 3;
+          }
+          _this.params.sort = sort+" "+type;
+        }
+        _this.ajaxCourseList(_this.params);
+      },
+      search(){
+        let _this = this;
+        _this.sort.productId = "";
+        _this.sort.isA = 5;
+        _this.params={
+          name:_this.sort.keyWords,
+          productId:"",
+          sort:""
+        }
+        _this.ajaxCourseList(_this.params);
+      },
+      ajaxCourseList(params){
+        let _this = this;
+        this.$http.get('course/listbycond', {
+          params:params
+        }).then(function (response) {
+          console.log(response)
+          if(0 == response.data.returnCode && response.data.data) {
+            _this.courses = response.data.data?response.data.data.map((item) => {
+              return {
+                courseId: item.courseId,
+                courseTitle:item.courseTitle,
+                visitors:item.visitors,
+                createTime:item.createTime,
+                courseType:item.courseType,
+                courseDesc:item.courseDesc,
+                courseLogo:_this.GLOBAL.BASE_URL+"course/logo/"+ item.courseId,
+                productName:item.productName
+              }
+            }):[];
+          }else{
+            _this.$message('获取API列表失败！');
+          }
+        }).catch(function (error) {});
+      },
+      // watch: {
+      //   '$route'(to, from) {
+      //     if (to.name === 'Learn') {
+      //       this.getCourseList();//获取所有课程信息
+      //       this.getProductList();
+      //       window.scrollTo(0,0);//初始化页面在最顶部
+      //     }
+      //   }
+      // }
+    }
+  }
+</script>
+
+<style scoped>
+  .learn-search {
+    height:48px;
+  }
+
+  .learn-search .grid-content {
+    margin-top: 9px;
+  }
+  .learn-source {
+    height:auto;
+  }
+
+  .font-color {
+    color:#373d41;
+    line-height: 30px;
+  }
+
+  .el-card {
+    height: 120px;
+    color:#373d41;
+  }
+
+  .el-card__body>div .bottom{
+    font-size: 12px;
+    margin-top: 14px;
+  }
+
+
+  .learn-title .icon {
+    height: 30px;
+    width: 30px;
+    float: left;
+    margin-right: 10px;
+  }
+
+  .learn-source .grid-content{
+    text-align: left;
+  }
+
+  .case-item {
+    width: 1140px;
+    margin: 0 auto;
+    padding: 40px 0;
+  }
+
+  .case-item-border {
+    border-bottom: 1px solid #e5e5e5;
+  }
+
+  .learn-source img {
+    height: 280px;
+    width: 520px;
+    margin-bottom: 20px;
+  }
+  .course-content {
+    margin-right: 16px;
+  }
+
+  .course-desc {
+    height: 55px;
+    overflow: hidden;
+  }
+  .case-item .el-button {
+    margin-top: 30px;
+    width: 206px;
+    height: 44px;
+  }
+  .el-button a {
+    color: #409EFF;
+    text-decoration: none;
+  }
+
+  .caret-wrapper {
+    display: inline-flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    width: 16px;
+    vertical-align: middle;
+    cursor: pointer;
+    overflow: initial;
+    position: relative;
+  }
+
+  .cell {
+    width: 124px;
+    float: left;
+    border-right: 1px solid #e5e5e5;
+    margin-top: 5px;
+    cursor: pointer;
+  }
+
+   .sort-caret.ascending {
+     border-bottom-color: #c0c4cc;
+     top: -10px;
+  }
+   .sort-caret.descending {
+     border-top-color: #c0c4cc;
+     bottom: -11px;
+
+  }
+   .sort-caret{
+      border: 5px solid transparent;
+      position: absolute;
+      left: 6px;
+  }
+
+  .el-select {
+    width: 140px;
+    float: left;
+  }
+
+  .el-input {
+    height: 32px;
+    width: 200px;
+  }
+
+  .ask {
+    color: #0084ff;
+  }
+   .ascending.active {
+     border-bottom-color: #0084ff!important;
+   }
+
+  .descending.active {
+    border-top-color: #0084ff!important;
+  }
+
+  .border-right {
+    border-right: 1px solid #e5e5e5;
+  }
+
+  .search-button {
+    width: 60px;
+    height: 31px;
+    padding: 0;
+  }
+
+
+</style>
