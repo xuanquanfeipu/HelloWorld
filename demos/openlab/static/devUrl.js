1a795a60397a83b4b7ad139dfee08f6a47f8e60c
haiyang <hhy5277@126.com>
2018-09-20 14:26:08 +0800

    Create devUrl.js

---
+++ demos/openlab/static/devUrl.js
@@ -0,0 +1,11 @@
+(function (env) {
+  const devUrl = {
+    // BASE_URL: "http://10.47.154.210:8181/isvportal/",
+    BASE_URL: "http://10.47.154.204:8181/isvportal/",
+    // BASE_URL: "http://10.41.166.82:8181/isvportal/",
+    // BASE_URL: "http://10.47.202.98:8181/isvportal/",
+    // BASE_URL: "http://10.47.151.220:/8181/isvportal/",
+    // BASE_URL: "http://10.47.151.211:8181/isvportal/"
+  };
+  return devUrl;
+})('env');
