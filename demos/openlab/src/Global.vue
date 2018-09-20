35337826cfc7d725eab707e89a392ddbee105b77
haiyang <hhy5277@126.com>
2018-09-20 15:58:06 +0800

    Create Global.vue

---
+++ demos/openlab/src/Global.vue
@@ -0,0 +1,7 @@
+<script>  
+  const BASE_URL = 'http://10.47.154.210:8181/isvportal/';  
+  // const BASE_URL = "http://10.41.166.82:8181/isvportal/";
+  export default{  
+    BASE_URL  
+  }  
+</script>  
