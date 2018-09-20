d2acd59818b0694c7a4f2b9fcc2bed9a56aaeb50
haiyang <hhy5277@126.com>
2018-09-20 16:17:56 +0800

    Create CopyRight.vue

---
+++ demos/openlab/src/components/CopyRight.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>©1998-2018中兴通讯股份有限公司 版权所有 <a style="text-decoration: none" href="http://www.miibeian.gov.cn/publish/query/indexFirst.action">粤ICP备11108162</a><a style="text-decoration: none" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030502000445">粤公网安备44030502000445</a></div>
+</template>
+<style scoped>
+div{
+  width:100%;
+  text-align:center;
+  /*box-shadow:0 1px 0 0 #1F2124 inset;*/
+  padding-top: 30px;
+  color: rgba(240,240,240,0.9);
+  font-size: 12px;
+}
+
+div span,div a{
+  color:#fff;
+  margin-left: 10px;
+}
+</style>
