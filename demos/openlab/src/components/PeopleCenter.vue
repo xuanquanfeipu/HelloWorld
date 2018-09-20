90fa84753d71e7c94d12d80a7abd72063384664d
haiyang <hhy5277@126.com>
2018-09-20 16:24:44 +0800

    Create PeopleCenter.vue

---
+++ demos/openlab/src/components/PeopleCenter.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-container style="margin:10px">
+        <el-aside width="200px">
+            <people-center-nav></people-center-nav>
+        </el-aside>
+        <el-main>
+            <router-view/>
+        </el-main>
+    </el-container>
+</template>
+<script>
+import PeopleCenterNav from '@/components/PeopleCenterNav'
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    components:{
+        PeopleCenterNav
+    },
+}
+</script>
+<style scoped>
+    .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+    }
+  
+    .el-aside {
+    background-color: #fff;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+    }
+  
+    body > .el-container {
+    margin-bottom: 40px;
+    }
+  
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+    }
+  
+    .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+    }
+</style>
+
