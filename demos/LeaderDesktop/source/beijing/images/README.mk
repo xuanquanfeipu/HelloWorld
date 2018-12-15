9e0b24864c5a76081f992a9034b49f89e11cc47a
haiyang <hhy5277@126.com>
2018-12-15 19:47:28 +0800

    Create README.mk

---
+++ demos/LeaderDesktop/source/beijing/images/README.mk
@@ -0,0 +1,12 @@
+Base64目前主要用于HTML5、移动开发等不考虑IE6的场景中。
+
+Base64格式
+data:[][;charset=][;base64],
+
+Base64 在CSS中的使用
+.demoImg{ background-image: url("data:image/jpg;base64,/9j/4QMZRXhpZgAASUkqAAgAAAAL...."); }
+
+Base64 在HTML中的使用
+<img width="40" height="30" src="data:image/jpg;base64,/9j/4QMZRXhpZgAASUkqAAgAAAAL...." />
+
+##此目录的图片可以通过BASE64转换工具转换成对应的图片
