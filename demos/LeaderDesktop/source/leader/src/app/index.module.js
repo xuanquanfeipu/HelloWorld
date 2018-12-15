92928c1de9676bd2c0413e95ec7537925ee6d6da
haiyang <hhy5277@126.com>
2018-12-15 20:27:04 +0800

    Create index.module.js

---
+++ demos/LeaderDesktop/source/leader/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('smartCore', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr', 'angular-carousel', 'baiduMap', 'ui.date', 'oitozero.ngSweetAlert', 'angular-loading-bar', 'ng-echarts', 'ui.router.tabs', 'nvd3', 'ng-token-auth', 'angular-md5']);
+
+})();
